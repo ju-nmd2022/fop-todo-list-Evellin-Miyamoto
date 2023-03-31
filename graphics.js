8e9e19b76576e76da73c411d4a08e2993070297a
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2023-03-31 10:58:20 +0200

    Fixed a bug
    - Fixed the bug creating a function for the scenery. Now the clouds are animated properly.

--- graphics.js
+++ graphics.js
@@ -1,9 +1,11 @@
-background(255, 255, 255);
-noStroke();
-fill(254, 234, 250);
-rect(220, 120, 500, 600, 40);
-fill(222, 226, 255);
-rect(200, 100, 500, 600, 40);
+function scenery() {
+  background(255, 255, 255);
+  noStroke();
+  fill(254, 234, 250);
+  rect(220, 120, 500, 600, 40);
+  fill(222, 226, 255);
+  rect(200, 100, 500, 600, 40);
+}
 
 //Code inspired by the night sky - Garrit's class
 let clouds = [];
@@ -17,20 +19,23 @@ for (let i = 0; i < 20; i++) {
   clouds.push(cloud);
 }
 
-function cloudDraw(x, y) {
-  fill(255, 255, 255);
-  ellipse(x, y, 20);
-  ellipse(x + 15, y, 20);
-  ellipse(x + 30, y, 20);
-  ellipse(x + 15, y - 5, 20);
+function cloudDraw(x, y, s) {
+  push();
+  ellipse(x, y, s);
+  ellipse(x + 15, y, s);
+  ellipse(x + 30, y, s);
+  ellipse(x + 15, y - 5, s);
+  pop();
 }
 
 function draw() {
+  scenery();
   noStroke();
 
   for (let cloud of clouds) {
-    fill(216, 187, 255, Math.abs(Math.sin(cloud.alpha)));
-    cloudDraw(cloud.x, cloud.y, 3);
-    cloud.alpha = cloud.alpha + 0.02;
+    fill(255, 255, 255, Math.abs(Math.sin(cloud.alpha)) * 255);
+    cloudDraw(cloud.x, cloud.y, 20);
+    //ellipse(cloud.x, cloud.y, 20);
+    cloud.alpha = cloud.alpha + 0.03;
   }
 }
