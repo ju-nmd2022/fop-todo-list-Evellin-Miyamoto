acde5e293ae87b88b053143d0a077e0d3d6d4c15
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2023-04-07 19:31:50 +0200

    Update with the add working
    - Adding items is working now and being added to the array
    - Created the done button
    - Created the delete button
    - Delete task is still not working properly

--- todolist.js
+++ todolist.js
@@ -1,35 +1,64 @@
 /* To Do List
 Foundations of Programming - Jönköping University
 Evellin Miyamoto */
-let inputList;
+
+// Variables and array
 let addButton;
 let finalList;
 let todoList = [];
 
+// Load handler learned from the class
 function loadHandler() {
-  let inputList = document.getElementById("textBox");
   let addButton = document.getElementById("addItemButton");
-  let finalList = document.getElementById("finalList");
   addButton.addEventListener("click", addNewItem);
-
-  for (let inputValue of todoList) {
-    const todoElement = document.createElement("li");
-    todoElement.innerText = inputValue;
-    finalList.appendChild(todoElement);
-  }
 }
 
+// function to add a new item
 function addNewItem() {
   let inputValue = document.getElementById("textBox").value;
-  document.getElementById("finalList").innerHTML = inputValue;
+  let finalList = document.getElementById("finalList");
+  //creating list
+  const todoElement = document.createElement("li");
+  todoElement.innerText = inputValue;
+  // to make the input blank after adding to the list
+  document.getElementById("textBox").value = "";
 
+  //creating the done button
+  const doneElement = document.createElement("button");
+  doneElement.innerText = "✅";
+  todoElement.appendChild(doneElement);
+  doneElement.classList.add("doneElement");
+  doneElement.addEventListener("click", () => {
+    todoElement.style.textDecoration = "line-through";
+  });
+
+  //creating the delete button
+  const deleteElement = document.createElement("button");
+  deleteElement.innerText = " ❌";
+  deleteElement.classList.add("deleteElement");
+  todoElement.appendChild(deleteElement);
+  deleteElement.addEventListener("click", deleteTask);
+  console.log(this);
+
+  //if conditions to add or not something to the list
   if (inputValue === "") {
     alert("Your textbox looks empty...");
   } else {
     todoList.push(inputValue);
+    //had to put in here otherwise it was adding blank task
+    finalList.appendChild(todoElement);
     textBox.innerText = "";
   }
   console.log(todoList);
 }
 
+function deleteTask() {
+  const element = this.parentNode;
+  const todoElement = element.querySelector("li");
+  const task = todoElement.value;
+  const taskIndex = todoList.indexOf(task);
+  todoList.splice(taskIndex, 1);
+
+  element.parentNode.removeChild(element);
+}
 window.addEventListener("load", loadHandler);
