289ae7bddac8afa1e6779c2e569bd51f9ef1eac0
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2023-04-07 16:17:12 +0200

    Changed and organised the code
    - After getting help from Garrit during the labs, I changed the structure of the code.
    - Organised the loadHandler
    - Set the list into an array and the function to add items to the array

--- todolist.js
+++ todolist.js
@@ -1,49 +1,35 @@
 /* To Do List
 Foundations of Programming - Jönköping University
 Evellin Miyamoto */
-
-// let inputList = document.getElementById("inputList");
-// let addItemButton = document.getElementById("addItemButton");
-// let finalList = document.getElementById("finalList");
-// let todoList = [];
-
-// for (let inputValue of todoList) {
-//   const todoElement = document.createElement("li");
-//   const inputValue = inputList.value;
-//   todoElement.innerText = inputValue;
-//   addItemButton.onclick = addNewItem;
-//   todoElement.classList.add("todoItems");
-
-//   finalList.appendChild(todoElement);
-// }
-
-// function addNewItem() {
-//   finalList.appendChild(this);
-// }
-
+let inputList;
+let addButton;
+let finalList;
 let todoList = [];
-function clickHandler() {
-  const inputList = document.getElementById("inputList");
-  const addButton = document.getElementById("addItemButton");
-  const inputValue = inputList.value;
-
-  let itemElement = document.createElement("li");
-  itemElement.classList.add("itemElement");
-  itemElement.innerText = inputValue;
-  finalList.appendChild(itemElement);
-
-  let doneElement = document.createElement("button");
-  doneElement.innerText = "✅";
-  itemElement.appendChild(doneElement);
-
-  let deleteElement = document.createElement("button");
-  deleteElement.innerText = " ❌";
-  deleteElement.classList.add("deleteElement");
-  itemElement.appendChild(deleteElement);
-}
 
 function loadHandler() {
-  const addButton = document.getElementById("addItemButton");
-  addButton.addEventListener("click", clickHandler);
+  let inputList = document.getElementById("textBox");
+  let addButton = document.getElementById("addItemButton");
+  let finalList = document.getElementById("finalList");
+  addButton.addEventListener("click", addNewItem);
+
+  for (let inputValue of todoList) {
+    const todoElement = document.createElement("li");
+    todoElement.innerText = inputValue;
+    finalList.appendChild(todoElement);
+  }
 }
+
+function addNewItem() {
+  let inputValue = document.getElementById("textBox").value;
+  document.getElementById("finalList").innerHTML = inputValue;
+
+  if (inputValue === "") {
+    alert("Your textbox looks empty...");
+  } else {
+    todoList.push(inputValue);
+    textBox.innerText = "";
+  }
+  console.log(todoList);
+}
+
 window.addEventListener("load", loadHandler);
