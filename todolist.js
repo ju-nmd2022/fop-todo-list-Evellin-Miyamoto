7f6c0b911a3df8db31941f7d302fad8c1d97e932
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2023-04-10 21:12:39 +0200

    Done Item saved to Local Storage
    -Done list are now saved and shown on the list
    - Delete items are now working properly with the local storage

--- todolist.js
+++ todolist.js
@@ -7,7 +7,10 @@ Evellin Miyamoto */
 // Variables and array
 let addButton;
 let finalList;
+//Creating array to store the list of items to be done
 let todoList = [];
+//Creating array to store the items already done
+let doneList = [];
 
 // Load handler learned from the class
 function loadHandler() {
@@ -22,13 +25,21 @@ https://stackoverflow.com/questions/72556032/localstorage-for-saving-tasks-of-to
 https://freshman.tech/todo-list/
 and got help from my husband to understand*/
 function getItemsFromJson() {
-  const jsonString = localStorage.getItem("todoList");
-  if (jsonString) {
-    let conversionToJS = JSON.parse(jsonString);
-    for (let item in conversionToJS) {
-      console.log(conversionToJS[item]);
-      todoList.push(conversionToJS[item]);
-      console.log(todoList);
+  const jsonTodoItems = localStorage.getItem("todoList");
+  const jsonDone = localStorage.getItem("doneList");
+
+  if (jsonDone) {
+    //translating the item from JSON to JS
+    let conversionItemDone = JSON.parse(jsonDone);
+    for (let item in conversionItemDone) {
+      doneList.push(conversionItemDone[item]);
+    }
+  }
+
+  if (jsonTodoItems) {
+    let conversionTodoItems = JSON.parse(jsonTodoItems);
+    for (let item in conversionTodoItems) {
+      todoList.push(conversionTodoItems[item]);
     }
   }
   //Process to create elements for the list
@@ -41,13 +52,23 @@ function getItemsFromJson() {
     todoElement.appendChild(spanElement);
     finalList.appendChild(todoElement);
 
+    if (doneList.includes(spanElement.innerText)) {
+      todoElement.style.textDecoration = "line-through";
+    }
     // Creating the done button
     const doneElement = document.createElement("button");
     doneElement.innerText = "✅";
     todoElement.appendChild(doneElement);
     doneElement.classList.add("doneElement");
     doneElement.addEventListener("click", () => {
-      todoElement.style.textDecoration = "line-through";
+      //This only adds the items that are not already in the doneList array to avoid duplicates
+      // Not reference - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_NOT
+      // includes reference - https://www.w3schools.com/jsref/jsref_includes_array.asp
+      if (!doneList.includes(spanElement.innerText)) {
+        doneList.push(spanElement.innerText);
+        todoElement.style.textDecoration = "line-through";
+        localStorage.setItem("doneList", JSON.stringify(doneList));
+      }
     });
 
     // Creating the delete button
@@ -79,7 +100,11 @@ function addNewItem() {
   todoElement.appendChild(doneElement);
   doneElement.classList.add("doneElement");
   doneElement.addEventListener("click", () => {
-    todoElement.style.textDecoration = "line-through";
+    if (!doneList.includes(spanElement.innerText)) {
+      doneList.push(spanElement.innerText);
+      todoElement.style.textDecoration = "line-through";
+      localStorage.setItem("doneList", JSON.stringify(doneList));
+    }
   });
 
   // Creating the delete button
@@ -107,10 +132,17 @@ function deleteTask() {
   const todoElement = element.querySelector("span");
   const task = todoElement.innerText;
 
+  //Delete item from array todoList
   const taskIndex = todoList.indexOf(task);
   todoList.splice(taskIndex, 1);
 
+  //Delete item from array doneList
+  const doneIndex = doneList.indexOf(task);
+  doneList.splice(doneIndex, 1);
+
   element.parentNode.removeChild(element);
   localStorage.setItem("todoList", JSON.stringify(todoList));
+  localStorage.setItem("doneList", JSON.stringify(doneList));
 }
+
 window.addEventListener("load", loadHandler);
